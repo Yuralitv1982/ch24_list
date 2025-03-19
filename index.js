84ce20c0e96358853fb23b69d3c943e05131b10e
Yuralitv1982 <yuralitv02@gmail.com>
2025-03-19 20:57:04 +0200

    callback pattern in Node

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const fs = require('fs');
+
+fs.readdir('.', (err, filenames) => {
+   //either
+   // err === an error object, which means something went wrong
+   // or
+   // err === nul, which means everything is OK
+
+   if (err) {
+      // error handling code here
+      console.log(err);
+      // return new Error(err)
+   }
+
+   console.log(filenames);
+});
